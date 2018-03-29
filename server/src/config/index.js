a316ee388176f5905e78423c1f17883bcb4ed94d
Lei Hua <leihua2009@gmail.com>
2018-03-28 22:58:48 -0400

    more dependencies

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,6 +2,13 @@ const config = {
   server: {
     port: process.env.PORT || 8081,
   },
+  graphql: {
+    endpoint: 'graphql',
+    viewer: 'graphiql',
+  },
+  database: {
+    uri: 'mongodb://localhost:27017/instagramcolon',
+  },
 }
 
 export default config
